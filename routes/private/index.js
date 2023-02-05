76dea2f991ea2f32cbe9ee6d0232970323c4c411
ihaio <ihaio95@gmail.com>
2023-02-04 22:24:44 -0300

    verify token

--- routes/private/index.js
+++ routes/private/index.js
@@ -2,11 +2,17 @@ const ClientsRoutes = require("./Clients.routes");
 const JobsRoutes = require("./Jobs.routes");
 const { expressjwt } = require("express-jwt");
 const { Router } = require("express");
+const { verifyToken } = require("../../middleware/authRoutes");
 const jwtSecret = process.env.JWT_SECRET;
 
 const router = Router();
 
-router.use(expressjwt({ secret: jwtSecret, algorithms: ["HS256"] }));
+// router.use(verifyToken);
+
+router.use(
+  expressjwt({ secret: jwtSecret, algorithms: ["HS256"] }),
+  verifyToken
+);
 
 router.use("/clients", ClientsRoutes, JobsRoutes);
 
