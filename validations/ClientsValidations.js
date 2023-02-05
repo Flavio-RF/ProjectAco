92633a73c0020e573be20843a7fd6859e7df38b4
ihaio <ihaio95@gmail.com>
2023-02-04 22:28:00 -0300

    change validations

--- validations/ClientsValidations.js
+++ validations/ClientsValidations.js
@@ -2,17 +2,17 @@ const { body, param } = require("express-validator");
 
 module.exports = {
   validateClient: [
-    body("name").notEmpty().exists().withMessage("the field can't be empty."),
-    body("address")
+    body("name")
       .notEmpty()
-      .exists()
-      .withMessage("the field can't be empty."),
-    body("zone").notEmpty().withMessage("the field can't be empty.").exists(),
-    body("email")
-      .isEmail()
-      .withMessage("Invalid Email")
+      .withMessage("the field can't be empty.")
       .exists()
       .withMessage("the field can't be undefined."),
+    body("address")
+      .notEmpty()
+      .withMessage("the field can't be empty.")
+      .exists(),
+    body("zone").exists().withMessage("the field can't be undefined."),
+    body("email").exists().withMessage("the field can't be undefined."),
     body("phone").exists().withMessage("the field can't be undefined."),
     body("mobile").exists().withMessage("the field can't be undefined."),
   ],
